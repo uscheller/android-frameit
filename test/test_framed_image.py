eff0e54d13f20bc1dacfffd8aed1e084c293c62b
Ulrich Scheller <mail@ulrich-scheller.de>
2020-11-17 11:19:11 +0100

    Fix unit tests.

--- test/test_framed_image.py
+++ test/test_framed_image.py
@@ -1,7 +1,5 @@
-import os
 import unittest
 from framed_image import FramedImage
-from translations import Translations
 
 
 class TranslationsTest(unittest.TestCase):
@@ -9,13 +7,8 @@ class TranslationsTest(unittest.TestCase):
         """
         Just testing that it does not crash.
         """
-        translations = Translations("default.json")
         screenshot = "example_screenshots/en_GB/screenshot_phone.png"
-        root, file = os.path.dirname(screenshot), os.path.basename(screenshot)
-        FramedImage(screen_shot=screenshot, output_name="should_not_be_saved.png") \
-            .add_text(translations.get_title(root, file), translations.get_message(root, file),
-                      title_font='fonts/MYRIADPRO-BOLDCOND.otf',
-                      text_font='fonts/OpenSansCondensed-Light.ttf')
+        FramedImage(screen_shot=screenshot, output_name="should_not_be_saved.png")
 
 
 if __name__ == '__main__':
